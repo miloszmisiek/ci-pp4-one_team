0273e045d5c56784f5fe9ec7f6e01a7be0d3e872
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-06-05 14:48:50 +0200

    Add forms.py in users app

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import CustomUser
-
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+
+admin.site.register(CustomUser, CustomUserAdmin)
