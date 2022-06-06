91a38339f7b987c47ef8602c1fb61856b8c5296e
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-06-06 20:29:48 +0200

    Add tasks template with functionality

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Task, Comment
 
 # Create your views here.
 
 def profile_home(request):
     """ A view to return the index page """
-    return render(request, 'tasks/profile_home.html')
+
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'tasks/profile_home.html', context)
