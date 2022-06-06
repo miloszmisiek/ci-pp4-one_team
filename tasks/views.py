4e4dd5e614e20fd4d6481c4504011380804067f2
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-06-06 20:09:30 +0200

    Add tasks home template

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def profile_home(request):
+    """ A view to return the index page """
+    return render(request, 'tasks/profile_home.html')
