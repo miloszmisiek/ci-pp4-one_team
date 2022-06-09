8aaca0beda5a346474a52189c4e4c940ea3ed8aa
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-06-09 19:08:36 +0200

    Edit home views to include difference for registered users

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from tasks.views import profile_home
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
-    return render(request, 'home/index.html')
+    return profile_home(request) if request.user.is_authenticated else render(request, 'home/index.html')
