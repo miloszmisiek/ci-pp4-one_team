04e403a64049b44e3920ee813ccd813dd10eb90f
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-06-12 18:58:32 +0200

    Add clear btn functionality

--- tasks/views.py
+++ tasks/views.py
@@ -10,26 +10,19 @@ from .forms import AddTask
 def profile_home(request):
     """ A view to return the tasks home page """
 
-    # show_completed = True
     tasks = Task.objects.all()
 
     if request.GET:
         if 'months' in request.GET:
             months = request.GET['months']
             tasks = tasks.filter(created_on__month=months)
-        if 'clear_completed' in request.GET:
-            tasks = tasks.exclude(status=1)
 
+    if request.POST:
+        tasks = tasks.exclude(status=1)
 
-    # if request.POST:
-    #     show_completed = False
-
-    # if not show_completed:
-    #     tasks = tasks.exclude(status=1)
 
     context = { 
         'tasks': tasks,
-        # 'show_completed':show_completed,
     }
     return render(request, 'tasks/profile_home.html', context)
 
